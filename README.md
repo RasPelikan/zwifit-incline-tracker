0ded60a2fde41cbc208e1dce24cc6eedaabebe02
RasPelikan <raspelikan@users.noreply.github.com>
2020-03-28 20:42:58 +0000

    First version

--- README.md
+++ README.md
@@ -1,2 +1,48 @@
 # zwifit-incline-tracker
-Set the incline of your Zwifit connected treadmill according your current Zwift session
+
+Zwift does not send any "set incline" commands to a treadmill. If you wish to adapt your treadmills incline automatically to the gain shown in your current Zwift session and you are using [Zwifit](https://github.com/dawsontoth/zwifit) to connect your treadmill to Zwift then you can use this software
+for that purpose.
+
+# Safety and claims
+
+The treadmills' incline mechanism is driven by a powerful engine. To incline or to decline opens and closes air gaps around your treadmill. It is possible to hurt yourself or a person nearby or damage anything getting into such an air gap. To avoid any injury do not start this software and leave your treadmill alone! I take no responsibility for any injury or damage caused by changing the incline according to your current Zwift session by this software.
+
+*Hint:* Updates to the incline of the treadmill are only processed by Zwifit if your treadmill is in the active
+state. If there is a problem then pause the treadmill or simply pull the safety key.
+
+# Usage
+
+## Prerequisites
+
+This software uses the module [zwift-packet-monitor](https://www.npmjs.com/package/@zwfthcks/zwift-packet-monitor)
+to track status updates from Zwift. It is based on the pcap-library which has to be installed first:
+
+* On Windows this use [WinPcap](https://www.winpcap.org/), [Win10PCap](http://www.win10pcap.org/) or [Npcap](https://nmap.org/npcap/).
+* On MacOS the pcap-library is installed by default.
+* On Linux systems install libpcap e.g. [Raspberry Pi](https://zoomadmin.com/HowToInstall/UbuntuPackage/libpcap-dev).
+
+## Installation
+
+*This software needs to run on the same computer as your Zwift is running!*
+
+Run this commands to install the software:
+
+```bash
+git clone https://github.com/RasPelikan/zwifit-incline-tracker.git
+cd zwifit-incline-tracker
+npm install
+```
+
+## Running
+
+Once installed you have to start the software evertime you start your Zwift run session. Therefor go to the installation directory and use this command:
+
+```bash
+npm start
+```
+
+*This will only succeed if you have already started Zwifit before!*
+
+## Hints
+
+1. On startup the software tries to find Zwifit in your local network. If it is running but could not be found then retry starting.
---
+++ app.js
@@ -0,0 +1,97 @@
+const Netmask = require('netmask').Netmask;
+const socketio = require('socket.io-client');
+const ZwiftPacketMonitor = require('@zwfthcks/zwift-packet-monitor');
+const internalIp = require('internal-ip');
+const net = require('net');
+const Socket = net.Socket;
+
+const minDistance = 3;
+let lastDistance = 0;
+let lastAltitude = 0;
+let currentIncline = 0;
+
+Array.prototype.asyncFilter = async function(f) {
+	const array = this;
+	const booleans = await Promise.all(array.map(f));
+	return array.filter((x, i) => booleans[i]);
+}
+
+async function run() {
+
+	console.log('Scanning your network for Zwifit...');
+	const ips = [];
+	const myIP = internalIp.v4.sync();
+	const block = new Netmask(myIP + '/24');
+	block.forEach((ip) => ips.push(ip));
+
+	const tests = await Promise.all(ips
+			.map(ip => testPortConnectable(ip, 1337)));
+	const clients = tests
+			.filter(test => test.result)
+			.map(test => socketio(`http://${test.ip}:1337`));
+
+	if (!clients || (clients.length === 0)) {
+		console.log('Did not found Zwifit on any IP address of your local network running at port 1337 :-(');
+		process.exit(1);
+	} else {
+		console.log('Found Zwifit at ', clients.map(client => client.io.uri));
+	}
+
+	console.log(`Tracking Zwift at ${myIP}...`);
+	const monitor = new ZwiftPacketMonitor(myIP);
+	monitor.on('outgoingPlayerState', (playerState, serverWorldTime) => {
+		
+			if (lastDistance && (playerState.distance > (lastDistance + minDistance))) {
+				const angle = Math.asin((playerState.altitude - lastAltitude) / (200 * (playerState.distance - lastDistance)));
+				const data = {
+					zwiftIncline: Math.round(200 * Math.tan(angle)) / 2
+				};
+				if (data.zwiftIncline !== currentIncline) {
+					console.log(data.zwiftIncline, playerState);
+					clients.forEach(client => client.emit('message', JSON.stringify({ event: 'control', data })));
+					currentIncline = data.zwiftIncline;
+				}
+					
+				lastAltitude = playerState.altitude;
+				lastDistance = playerState.distance;
+			} else if (!lastDistance || (playerState.distance < lastDistance)) {
+				lastAltitude = playerState.altitude;
+				lastDistance = playerState.distance;
+			}
+			
+		});
+	
+	monitor.start();
+	
+}
+run();
+
+function testPortConnectable(ip, port) {
+	
+	return new Promise(resolve => {
+
+		const socket = new Socket();
+		let result = false;
+	
+		socket.on('connect', () => {
+			result = true;
+			socket.destroy();
+		});
+	
+		socket.setTimeout(400);
+		socket.on('timeout', () => {
+			socket.destroy();
+		});
+		
+		socket.on('error', (error) => {
+		});
+	
+		socket.on('close', (exception) => {
+			resolve({ ip, result });
+		});
+	
+		socket.connect(port, ip);
+
+	});
+	
+}
